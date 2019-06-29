e14453a6003503046dd6f34a87ff4fc26e6b92b8
bianxinhuan <bianxinhuan@gmail.com>
2019-06-29 22:03:28 +0800

    第2章 可选1：优化选择排序，在每一轮中, 可以同时找到当前未处理元素的最大值和最小值

---
+++ 02-Sorting-Basic/Course Code(Java)/Optional-01-Optimized-Selection-Sort/src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+
+/**
+ * -
+ *
+ * @author bianxinhuan
+ * @date 2019-06-28 22:08:27
+ */
+public class Main {
+
+    /**
+     * 比较SelectionSort和InsertionSort两种排序算法的性能效率
+     * 优化后，插入排序比选择排序性能略好
+     * 对于有序性强的数组，插入排序远远优于选择排序
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        int N = 20000;
+
+        // 测试1 一般测试
+        System.out.println("Test for random array, size = " + N + " , random range [0, " + N + "]");
+
+        Integer[] arr1 = SortTestHelper.generateRandomArray(N, 0, N);
+        Integer[] arr2 = Arrays.copyOf(arr1, arr1.length);
+        Integer[] arr3 = Arrays.copyOf(arr1, arr1.length);
+
+        SortTestHelper.testSort(InsertionSort.class.getName(), arr1);
+        SortTestHelper.testSort(SelectionSort.class.getName(), arr2);
+        SortTestHelper.testSort(SelectionSort2.class.getName(), arr3);
+
+        System.out.println();
+
+        // 测试2 有序性更强的数组
+        System.out.println("Test for more ordered random array, size = " + N + " , random range [0,3]");
+
+        arr1 = SortTestHelper.generateRandomArray(N, 0, 3);
+        arr2 = Arrays.copyOf(arr1, arr1.length);
+        arr3 = Arrays.copyOf(arr1, arr1.length);
+
+        SortTestHelper.testSort(InsertionSort.class.getName(), arr1);
+        SortTestHelper.testSort(SelectionSort.class.getName(), arr2);
+        SortTestHelper.testSort(SelectionSort2.class.getName(), arr3);
+
+        System.out.println();
+
+        // 测试3 测试近乎有序的数组
+        int swapTimes = 100;
+        System.out.println("Test for nearly ordered array, size = " + N + " , swap time = " + swapTimes);
+
+        arr1 = SortTestHelper.generateNearlyOrderedArray(N, swapTimes);
+        arr2 = Arrays.copyOf(arr1, arr1.length);
+        arr3 = Arrays.copyOf(arr1, arr1.length);
+
+        SortTestHelper.testSort(InsertionSort.class.getName(), arr1);
+        SortTestHelper.testSort(SelectionSort.class.getName(), arr2);
+        SortTestHelper.testSort(SelectionSort2.class.getName(), arr3);
+    }
+}
